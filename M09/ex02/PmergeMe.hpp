7a8f34bb8c5d901e80ac84093b8e9267a113f126
post@albertvanandel.nl <you@example.com>
2025-02-07 23:42:37 +0100

    added verbose flag

--- M09/ex02/PmergeMe.hpp
+++ M09/ex02/PmergeMe.hpp
@@ -11,41 +11,41 @@ class PmergeMe
 	typedef typename C::iterator Citerator;
 	typedef typename C::value_type ValueType;
 
-	private:
-		PmergeMe();
+private:
+	PmergeMe();
 
-		C _container;
-		time_t _startTime;
-		time_t _endTime;
+	C _container;
+	time_t _startTime;
+	time_t _endTime;
 
-		// sort functions
-		size_t _jacobsthal(size_t n);
-		// void _sort()
+	// sort functions
+	size_t _jacobsthal(size_t n);
+	// void _sort()
 
-		// time functions
-		void _setStartTime();
-		void _setEndTime();
-		time_t _getElapsedTime();
+	// time functions
+	void _setStartTime();
+	void _setEndTime();
+	time_t _getElapsedTime();
 
-		// parsing
-		bool _isPositiveInteger(ValueType &number, const std::string &s);
+	// parsing
+	bool _isPositiveInteger(ValueType &number, const std::string &s);
 
-		// printing
-		void _printPairs(int depth);
-		void _printMerge(Citerator start, Citerator end, int pairSize);
+	// printing
+	void _printPairs(int depth);
+	void _printMerge(Citerator start, Citerator end, int pairSize);
 
-	public:
-		PmergeMe(int argc, char **argv);
-		void sort();
+public:
+	PmergeMe(int argc, char **argv);
+	void sort(bool verbose = false);
 
-		// iterators
-		typename C::iterator begin();
-		typename C::iterator end();
+	// iterators
+	typename C::iterator begin();
+	typename C::iterator end();
 };
 
 // SORT FUNCTIONS
 template <typename C>
-void PmergeMe<C>::sort()
+void PmergeMe<C>::sort(bool verbose)
 {
 	static int pairSize = 1;
 	if (pairSize == 1)
@@ -59,7 +59,8 @@ void PmergeMe<C>::sort()
 	Citerator start = _container.begin();
 	Citerator end = start + (pairSize * pairCount) - (isOdd * pairSize); // outer bound is amount of pairs * pair size minus the leftover
 
-	_printMerge(start, end, pairSize);
+	if (verbose)
+		_printMerge(start, end, pairSize);
 
 	// merge pairs and swap if necessary
 	for (Citerator it = start; it != end; it += pairSize * 2) // loop through pairs, iterator increases with pairsize * 2 so we skip the pairs we already merged
@@ -69,138 +70,141 @@ void PmergeMe<C>::sort()
 			std::swap_ranges(it, it + pairSize, it + pairSize); // swap the pairs;
 		}
 	}
-	std::cout << "Merged pairs of " << pairSize << std::endl;
-	_printMerge(start, end, pairSize);
-	std::cout << std::endl;
-
-	pairSize *= 2; // double the pair size
-	sort();		   // recursive call
-	pairSize /= 2; // halve the pair size after the recursive call
-
-	// _sort();
-	// _setEndTime();
-}
-
-// PRINT FUNCTIONS
-
-template <typename C>
-void PmergeMe<C>::_printMerge(Citerator start, Citerator end, int pairSize)
-{
-	int halfPair = 0;
-	int fullPair = 0;
-	std::cout << "Pairs of " << pairSize << ": ";
-	for (Citerator it = start; it != end; it++)
+	if (verbose)
 	{
-		if (fullPair == 0)
+		std::cout << "Merged pairs of " << pairSize << std::endl;
+		_printMerge(start, end, pairSize);
+		std::cout << std::endl;
+	}
+
+			pairSize *= 2; // double the pair size
+		sort(verbose);			   // recursive call
+		pairSize /= 2;	   // halve the pair size after the recursive call
+
+		// _sort();
+		// _setEndTime();
+	}
+
+	// PRINT FUNCTIONS
+
+	template <typename C>
+	void PmergeMe<C>::_printMerge(Citerator start, Citerator end, int pairSize)
+	{
+		int halfPair = 0;
+		int fullPair = 0;
+		std::cout << "Pairs of " << pairSize << ": ";
+		for (Citerator it = start; it != end; it++)
 		{
-			std::cout << "[";
-		}
-		if (halfPair == 0)
-		{
-			std::cout << "{";
-		}
-		std::cout << *it;
-		++fullPair;
-		++halfPair;
-		if (halfPair == pairSize)
-		{
-			std::cout << "}";
-			halfPair = 0;
-			if (fullPair == pairSize * 2)
+			if (fullPair == 0)
 			{
-				std::cout << "] ";
-				fullPair = 0;
+				std::cout << "[";
+			}
+			if (halfPair == 0)
+			{
+				std::cout << "{";
+			}
+			std::cout << *it;
+			++fullPair;
+			++halfPair;
+			if (halfPair == pairSize)
+			{
+				std::cout << "}";
+				halfPair = 0;
+				if (fullPair == pairSize * 2)
+				{
+					std::cout << "] ";
+					fullPair = 0;
+				}
+				else
+				{
+					std::cout << " - ";
+				}
 			}
 			else
 			{
-				std::cout << " - ";
+				std::cout << " ";
 			}
 		}
-		else
+		if (end != _container.end())
 		{
-			std::cout << " ";
+			for (Citerator it = end; it != _container.end(); it++)
+			{
+				std::cout << *it << " ";
+			}
 		}
+		std::cout << std::endl;
 	}
-	if (end != _container.end())
+
+	template <typename C>
+	std::ostream &operator<<(std::ostream &os, PmergeMe<C> &p)
 	{
-		for (Citerator it = end; it != _container.end(); it++)
+		for (typename C::iterator it = p.begin(); it != p.end(); it++)
 		{
-			std::cout << *it << " ";
+			os << *it << " ";
 		}
+		os << std::endl;
+		return os;
 	}
-	std::cout << std::endl;
-}
 
-template <typename C>
-std::ostream &operator<<(std::ostream &os, PmergeMe<C> &p)
-{
-	for (typename C::iterator it = p.begin(); it != p.end(); it++)
+	template <typename C>
+	void PmergeMe<C>::_printPairs(int depth)
 	{
-		os << *it << " ";
 	}
-	os << std::endl;
-	return os;
-}
 
-template <typename C>
-void PmergeMe<C>::_printPairs(int depth)
-{
-}
+	// ITERATORS
 
-// ITERATORS
-
-template <typename C>
-typename C::iterator PmergeMe<C>::begin()
-{
-	return _container.begin();
-}
-
-template <typename C>
-typename C::iterator PmergeMe<C>::end()
-{
-	return _container.end();
-}
-
-// HELPER FUNCTIONS
-
-template <typename C>
-bool PmergeMe<C>::_isPositiveInteger(ValueType &number, const std::string &s)
-{
-	try
+	template <typename C>
+	typename C::iterator PmergeMe<C>::begin()
 	{
-		number = std::stoi(s);
-		if (number < 0)
+		return _container.begin();
+	}
+
+	template <typename C>
+	typename C::iterator PmergeMe<C>::end()
+	{
+		return _container.end();
+	}
+
+	// HELPER FUNCTIONS
+
+	template <typename C>
+	bool PmergeMe<C>::_isPositiveInteger(ValueType & number, const std::string &s)
+	{
+		try
 		{
+			number = std::stoi(s);
+			if (number < 0)
+			{
+				return false;
+			}
+		}
+		catch (std::invalid_argument &e)
+		{
+			std::cerr << "Invalid argument: " << s << std::endl;
 			return false;
 		}
-	}
-	catch (std::invalid_argument &e)
-	{
-		std::cerr << "Invalid argument: " << s << std::endl;
-		return false;
-	}
-	catch (std::out_of_range &e)
-	{
-		std::cerr << "Out of range: " << s << std::endl;
-		return false;
-	}
-	return true;
-}
-
-// CONSTRUCTOR
-
-template <typename C>
-PmergeMe<C>::PmergeMe(int argc, char **argv)
-{
-	ValueType n;
-	for (int i = 1; i < argc; i++)
-	{
-		if (!_isPositiveInteger(n, argv[i]))
+		catch (std::out_of_range &e)
 		{
-			throw "Can't construct PmergeMe";
+			std::cerr << "Out of range: " << s << std::endl;
+			return false;
 		}
-		_container.push_back(n);
+		return true;
 	}
-}
+
+	// CONSTRUCTOR
+
+	template <typename C>
+	PmergeMe<C>::PmergeMe(int argc, char **argv)
+	{
+		ValueType n;
+		for (int i = 1; i < argc; i++)
+		{
+			if (!_isPositiveInteger(n, argv[i]))
+			{
+				throw "Can't construct PmergeMe";
+			}
+			_container.push_back(n);
+		}
+	}
 
 #endif
--- M09/ex02/main.cpp
+++ M09/ex02/main.cpp
@@ -3,11 +3,26 @@
 
 int main(int argc, char **argv)
 {
+
+	bool verbose = false; // verbose flag
+	int new_argc = 1; // counter to shift arguments
+
+	for (int i = 1; i < argc; ++i)
+	{
+		if (std::string(argv[i]) == "--verbose" || std::string(argv[i]) == "-v")
+		{
+			verbose = true; // set verbose to true
+		}
+		else
+		{
+			argv[new_argc++] = argv[i]; // else shift the argument to the left
+		}
+	}
 	try
 	{
-		PmergeMe<std::vector<int> > p(argc, argv);
-		std::cout << p;
-		p.sort();
+		PmergeMe<std::vector<int> > p(argc - verbose, argv); // create a PmergeMe object, passing the number of arguments and the arguments
+		std::cout << p; 
+		p.sort(verbose);
 	}
 	catch (const char *e)
 	{
