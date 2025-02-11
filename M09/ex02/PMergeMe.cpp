9aad0738e22ad5945ad1cf72a570a63f8899d6d3
post@albertvanandel.nl <you@example.com>
2025-02-11 21:47:26 +0100

    added mandatory cpp file

---
+++ M09/ex02/PMergeMe.cpp
@@ -0,0 +1,5 @@
+// I am here because I am mandatory, but the class is imlemented as a template so I am not used.
+// Implementing a template class is much harder than writing different functions for each type,
+// which was possible because CODAM allows the use of CPP above 98, and hence the use of the Iterator library,
+// containing functions like std::distance, std::advance, std::next, std::prev, etc.
+// Without these, writing a template class for different container types would be a nightmare.
